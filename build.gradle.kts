2dfa778aa99421621501e9c1d07d34aa0382d2f0
kahverengi <ahmet@duruer.dev>
2024-02-19 17:24:08 +0300

    feat: added license and building with jitpack support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
-    java
     kotlin("jvm") version "1.9.20"
     kotlin("kapt") version "1.9.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    `maven-publish`
 }
+
+group = "co.statu.parsek"
+version = project.findProperty("version") ?: "0.0.1"
 
 val pf4jVersion: String by project
 val vertxVersion: String by project
@@ -45,5 +48,51 @@ tasks.named("jar").configure {
 tasks {
     build {
         dependsOn(shadowJar)
+    }
+
+    shadowJar {
+        val pluginId: String by project
+        val pluginClass: String by project
+        val pluginProvider: String by project
+        val pluginDependencies: String by project
+
+        manifest {
+            attributes["Plugin-Class"] = pluginClass
+            attributes["Plugin-Id"] = pluginId
+            attributes["Plugin-Version"] = archiveVersion
+            attributes["Plugin-Provider"] = pluginProvider
+            attributes["Plugin-Dependencies"] = pluginDependencies
+        }
+
+        archiveFileName.set("$pluginId.jar")
+
+        dependencies {
+            exclude(dependency("io.vertx:vertx-core"))
+            exclude {
+                it.moduleGroup == "io.netty" || it.moduleGroup == "org.slf4j"
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "parsek-i8n"
+            url = uri("https://maven.pkg.github.com/StatuParsek/parsek-i18n")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME_GITHUB")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN_GITHUB")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("shadow") {
+            project.extensions.configure<com.github.jengelman.gradle.plugins.shadow.ShadowExtension> {
+                artifactId = "core"
+                component(this@create)
+            }
+        }
     }
 }
