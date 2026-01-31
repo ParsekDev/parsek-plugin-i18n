b8b2d4623c28e6adc23b4f1d5106929391b5732a
kahverengi <ahmet@duruer.dev>
2026-02-01 00:57:42 +0300

    feat: improved codebase

--- src/main/kotlin/co/statu/rule/plugins/i18n/I18nPlugin.kt
+++ src/main/kotlin/co/statu/rule/plugins/i18n/I18nPlugin.kt
@@ -1,5 +1,62 @@
 package co.statu.rule.plugins.i18n
 
+import co.statu.parsek.PluginEventManager
 import co.statu.parsek.api.ParsekPlugin
+import co.statu.parsek.api.config.PluginConfigManager
+import co.statu.rule.auth.AuthConfig
+import co.statu.rule.auth.AuthFieldManager
+import co.statu.rule.plugins.i18n.event.I18nEventListener
 
-class I18nPlugin : ParsekPlugin()
+class I18nPlugin : ParsekPlugin() {
+    override suspend fun onStart() {
+        val pluginConfigManager = PluginConfigManager(
+            this,
+            I18nConfig::class.java
+        )
+
+        pluginBeanContext.beanFactory.registerSingleton(
+            pluginConfigManager.javaClass.name,
+            pluginConfigManager
+        )
+
+        logger.info("Initialized plugin config")
+
+        val i18nSystem = I18nSystem(
+            vertx,
+            pluginConfigManager,
+            logger
+        )
+
+        registerSingletonGlobal(i18nSystem)
+
+        val i18nEventHandlers = PluginEventManager.getEventListeners<I18nEventListener>()
+
+        i18nEventHandlers.forEach { it.onReady(i18nSystem) }
+
+        val config = pluginConfigManager.config
+
+        if (!config.hookAuthPlugin) {
+            return
+        }
+
+        val authFieldManager = pluginBeanContext.getBean(AuthFieldManager::class.java)
+
+        authFieldManager.addRegisterField(
+            AuthConfig.Companion.RegisterField(
+                field = "lang",
+                isBlankCheck = true,
+                optional = false,
+                min = 0,
+                max = null,
+                regex = null,
+                unique = false,
+                upperCaseFirstChar = false,
+                hiddenToUI = false,
+                type = AuthConfig.Companion.RegisterField.Companion.Type.STRING,
+                onlyRegister = false
+            )
+        )
+
+        logger.info("Hooked into parsek-plugin-auth plugin")
+    }
+}
